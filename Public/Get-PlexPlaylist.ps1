a5731a93c1a40a4d08590161d919d29189e43851
bifa2 <54040596+bifa2@users.noreply.github.com>
2020-04-18 18:55:49 +0100

    Restructure slightly for easier reading

--- Public/Get-PlexPlaylist.ps1
+++ Public/Get-PlexPlaylist.ps1
@@ -22,37 +22,56 @@ function Get-PlexPlaylist
 
     $RestEndpoint   = "playlists/$ID"
 
+	#############################################################################
+	Write-Verbose -Message "Function: $($MyInvocation.MyCommand): Getting playlist(s)"
 	try 
 	{
-		if($AlternativeToken) { $Token = $AlternativeToken} else { $Token = $PlexConfigData.Token }
-		Write-Verbose -Message "Function: $($MyInvocation.MyCommand): Getting playlist(s)"
-		[array]$data = Invoke-RestMethod -Uri "$($PlexConfigData.Protocol)`://$($PlexConfigData.PlexServerHostname)`:$($PlexConfigData.Port)/$RestEndpoint`?`X-Plex-Token=$($Token)" -ErrorAction Stop
-		[array]$results = $data.MediaContainer.Playlist
-		
-
-		if($IncludeItems)
+		if($AlternativeToken)
+		{ 
+			$Token = $AlternativeToken
+		} 
+		else
 		{
-			foreach($playlist in $results)
-			{
-				$RestEndpoint   = "playlists/$($playlist.ratingKey)/items"
-				Write-Verbose -Message "Function: $($MyInvocation.MyCommand): Appending playlist item(s)"
-				try 
-				{
-					[array]$Items = Invoke-RestMethod -Uri "$($PlexConfigData.Protocol)`://$($PlexConfigData.PlexServerHostname)`:$($PlexConfigData.Port)/$RestEndpoint`?`X-Plex-Token=$($PlexConfigData.Token)" -ErrorAction Stop
-					$playlist | Add-Member -NotePropertyName 'Videos' -NotePropertyValue $Items.MediaContainer.Video
-				}
-				catch
-				{
-					throw $_
-				}
-			}
+			$Token = $PlexConfigData.Token
 		}
+
+		$data = Invoke-RestMethod -Uri "$($PlexConfigData.Protocol)`://$($PlexConfigData.PlexServerHostname)`:$($PlexConfigData.Port)/$RestEndpoint`?`X-Plex-Token=$($Token)" -ErrorAction Stop
+		if($data.MediaContainer.Size -eq 0)
+		{
+			return
+		}
+
+		[array]$results = $data.MediaContainer.Playlist
     }
     catch
     {
         throw $_
-    }
+	}
+	
 
+	#############################################################################
+	if($IncludeItems)
+	{
+		foreach($playlist in $results)
+		{
+			$RestEndpoint   = "playlists/$($playlist.ratingKey)/items"
+			Write-Verbose -Message "Function: $($MyInvocation.MyCommand): Appending playlist item(s)"
+			try 
+			{
+				[array]$Items = Invoke-RestMethod -Uri "$($PlexConfigData.Protocol)`://$($PlexConfigData.PlexServerHostname)`:$($PlexConfigData.Port)/$RestEndpoint`?`X-Plex-Token=$($PlexConfigData.Token)" -ErrorAction Stop
+				$playlist | Add-Member -NotePropertyName 'Videos' -NotePropertyValue $Items.MediaContainer.Video
+			}
+			catch
+			{
+				throw $_
+			}
+		}
+	}
+
+
+
+	#############################################################################
+	# Append type and return results
 	$results | ForEach-Object { $_.psobject.TypeNames.Insert(0, "PSPlex.Playlist") }
     return $results
 }
