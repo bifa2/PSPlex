a3ea3de5371349c487606470ec6572c5a3077244
Tony Hunt <TechnoTone@gmail.com>
2020-04-28 18:30:00 +0100

    Add Get-PlexGenre function

---
+++ Public/Get-PlexGenre.ps1
@@ -0,0 +1,50 @@
+﻿function Get-PlexGenre
+{  
+	[CmdletBinding()]
+	param(
+        [Parameter(Mandatory=$true)]
+        [Int]
+        $LibraryID,
+
+        [Parameter(Mandatory=$false)]
+        [PSObject]
+		$ID
+	)
+	
+	if($PlexConfigData.PlexServer -eq $Null)
+	{
+		throw "No saved configuration. Please run Get-PlexAuthenticationToken, then Save-PlexConfiguration first."
+	}
+
+	if($ID)
+	{
+		$RestEndpoint = "library/sections/$LibraryID/all?genre=$ID&X-Plex-Token=$($PlexConfigData.Token)"
+	}
+	else
+	{
+		$RestEndpoint = "library/sections/$LibraryID/genre`?`X-Plex-Token=$($PlexConfigData.Token)"
+	}    
+
+	try 
+	{
+		$data = Invoke-RestMethod -Uri "$($PlexConfigData.Protocol)`://$($PlexConfigData.PlexServerHostname)`:$($PlexConfigData.Port)/$RestEndpoint" -Method GET -ErrorAction Stop
+		if($data.MediaContainer.Size -eq 0)
+		{
+			return
+		}
+    }
+    catch
+    {
+        throw $_
+	}
+
+	if($ID)
+	{
+		return $data.MediaContainer
+	}
+	else
+	{
+		return $data.MediaContainer.Directory
+	}
+
+}
